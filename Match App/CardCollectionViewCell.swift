1a570210f23ac1941a356aec623633d1c40cc28d
huunghiaa <nghuunghiaa@gmail.com>
2019-04-01 22:17:36 +0700

    Nothinggg

--- Match App/CardCollectionViewCell.swift
+++ Match App/CardCollectionViewCell.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var frontImageView: UIImageView!
     
+    @IBOutlet weak var backImageView: UIImageView!
+    
+    var card:Card?
+    
+    func setCard(_ card:Card) {
+        // Keep track of the card that gets passed in
+        self.card = card
+        
+        frontImageView.image = UIImage(named: card.imageName)
+        
+        // Determine if the card is in a flipped up state of flipped down state
+        if card.isFlipped == true {
+            //Make sure the frontImageView is on top
+            UIView.transition(from: backImageView, to: frontImageView, duration: 0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        } else {
+            // Make sure the backImageView is on top
+            UIView.transition(from: frontImageView, to: backImageView, duration: 0, options: [.showHideTransitionViews, .transitionFlipFromLeft], completion: nil)
+        }
+    }
+    
+    func flip() {
+        UIView.transition(from: backImageView, to: frontImageView, duration: 0.3, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+    }
+    
+    func flipBack() {
+        UIView.transition(from: frontImageView, to: backImageView, duration: 0.3, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+    }
     
 }
--- Match App/ViewController.swift
+++ Match App/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var model = CardModel()
     var cardArray = [Card]()
@@ -18,8 +20,50 @@ class ViewController: UIViewController {
         
         //Call the getCards mothod of the card model
         cardArray = model.getCards()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
     }
 
+    // MARK: - UICollectionView Protocol Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // Get a CardCollectionViewCell object
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
+        
+        // Get the card that the collection view is trying to display
+        let card = cardArray[indexPath.row]
+        
+        // Set that card for the cell
+        cell.setCard(card)
+        return cell
+    }
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Get the card that the user selected
+        let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
+        
+        // Get the card that the user selected
+        let card = cardArray[indexPath.row]
+        
+        if card.isFlipped == false {
+            // Flip the card
+            cell.flip()
+            card.isFlipped = true
+        } else {
+            // Flip the card back
+            cell.flipBack()
+            card.isFlipped = false
+        }
+        
+       
+    }
 }
 
